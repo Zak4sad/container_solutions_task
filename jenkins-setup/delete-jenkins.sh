39e04ad477df98d6c2a4acab537a9456aee80102
Zak4sad <zakariakarim1616@gmail.com>
2022-08-20 21:52:17 +0100

    delete jenkins configuration and move it to a separate repository

--- jenkins-setup/delete-jenkins.sh
+++
@@ -1,6 +0,0 @@
-kubectl delete -f jenkins-service.yml
-kubectl delete -f jenkins-deployment.yml
-kubectl delete -f jenkins-persistent-volume-claim.yml
-kubectl delete -f jenkins-persistent-volume.yml
-kubectl delete namespace jenkins
-rm -rf /mnt/jenkins/*
--- jenkins-setup/install-jenkins.sh
+++
@@ -1,7 +0,0 @@
-kubectl create namespace jenkins
-kubectl apply -f jenkins-persistent-volume.yml
-kubectl apply -f jenkins-persistent-volume-claim.yml
-kubectl create sa jenkins --namespace jenkins
-kubectl create rolebinding jenkins-admin-binding --clusterrole=admin --serviceaccount=jenkins:jenkins --namespace=jenkins
-kubectl apply -f jenkins-deployment.yml
-kubectl apply -f jenkins-service.yml
